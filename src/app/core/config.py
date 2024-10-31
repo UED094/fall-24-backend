947050f563298df38832bcef7d59fe6b187b13ff
Ugur Emre Dogan <ue.dogan@gmail.com>
2024-10-30 18:59:15 -0700

    Added user models, schemas, routes and included the route in the app.

--- src/app/core/config.py
+++ src/app/core/config.py
@@ -1,14 +1,14 @@
+from functools import lru_cache
+
 from dotenv import load_dotenv
 from pydantic_settings import BaseSettings, SettingsConfigDict
-from pydantic import FilePath
-
-from functools import lru_cache
 
 load_dotenv()
 
 
 class Settings(BaseSettings):
     SQLALCHEMY_DATABASE_URL: str
+    SECRET_KEY: str
 
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
 
--- src/app/core/database.py
+++ src/app/core/database.py
@@ -1,16 +1,14 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
-from app.core.config import settings
+from sqlalchemy.orm import sessionmaker
 
+from app.core.config import settings
 
 SQLALCHEMY_DATABASE_URL = settings.SQLALCHEMY_DATABASE_URL
 
 # TODO: Change this to environment variable
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
+engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
