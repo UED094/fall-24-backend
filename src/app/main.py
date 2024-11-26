40ded7d8d445f70630a6b79575ea03700251112f
Ugur Emre Dogan <ue.dogan@gmail.com>
2024-11-25 20:42:25 -0800

    Added cors config.

--- src/app/main.py
+++ src/app/main.py
@@ -2,10 +2,19 @@ from fastapi import FastAPI
 
 from app.core.database import Base, engine
 from app.routes import api_router
+from fastapi.middleware.cors import CORSMiddleware
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(api_router)
 
